1632fc77c12eeaf3948424e5ca256d6d5c954b53
Daniel Lundby <daniellundby@Daniels-MacBook-Pro.local>
2017-08-22 16:14:15 -0600

    stopping point

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 var express = require('express');
-var methodOverride = require('menthod-override');
+var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+
+
+
+app.listen(port, ()=>{
+  console.log('served started on port', port);
+});
